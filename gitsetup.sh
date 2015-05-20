c8850e845eef5fb74138d5e22a33cf9ae148adbc
maartenkoning <websites@geoduo.nl>
2015-05-20 10:17:54 +0200

    url naar lokale server aangepast

--- gitsetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-echo # km_mapapp_debilt >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/maartenkoning/km_mapapp_debilt.git
-git push -u origin master
