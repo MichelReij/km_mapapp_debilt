b62ebffd30d0b67432324065d2abb1709efda403
maartenkoning <websites@geoduo.nl>
2015-09-01 13:38:43 +0200

    map buttons event aangepast voor android

--- www/js/map.js
+++ www/js/map.js
@@ -1,7 +1,14 @@
 var watchID = 0;
+//standard event for custom map controls
+var clickEvent = "click"; //default event
 
 
-var map = L.map('map').setView([52.10451, 5.17537], 15);
+var map = L.map('map',{zoomControl: false}).setView([52.10451, 5.17537], 15);
+
+//check if touch screen is used
+if (L.Browser.touch){
+    clickEvent = "touchend";
+}
 
 var openStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
@@ -22,35 +29,72 @@ openStreetMap_Mapnik.addTo(map);
 
 L.control.scale({imperial: false}).addTo(map);
 
+L.control.zoom({position: 'bottomright'}).addTo(map);
 
 
 
+/**
+ * popup marker for db info
+ * @param {jsonMarker} f
+ * @param {latlong} l
+ * @returns {undefined}
+ */
 function popUp(f, l) {
-    var out = [];
-    var userProp = "";
+    //var out = [];
+    //var userProp = "";
     if (f.properties) {
-        for (key in f.properties) {
-            userpropNoPlus = f.properties[key].toString().replace(/\+/g, ' ');
-            userProp = decodeURIComponent(userpropNoPlus);
-            out.push(key + ": " + userProp);
-        }
-        l.bindPopup(out.join("<br />"));
+        l.bindPopup(popUpHTML(f.properties));
     }
 }
+/**
+ * html layout for popup marker
+ * @param {geojsonData} popProperties
+ * @returns {String} html
+ */
+function popUpHTML(popProperties){
+    var htmlOut = "";
+    if (popProperties['kunstenaar'].length > 0){
+        htmlOut +="<p><b>";
+        htmlOut += popProperties['kunstenaar'];
+        htmlOut += "</b><p>";
+    }
+    htmlOut +="<table><tbody>";
+    
+    if (popProperties['adres'].length > 0){
+        htmlOut +="<tr class='popuptr'><td class='popuptd'><img alt='adres' width='28px' src='img/envelop.png'></td><td>";
+        htmlOut += decodeURIComponent(popProperties['adres']);
+        htmlOut += "</td></tr>";
+    }
+    if (popProperties['website'].length > 0){
+       htmlOut +="<tr class='popuptr'><td class='popuptd'><img alt='website' width='28px' src='img/www_globe.png'></td><td>";
+        htmlOut += popProperties['website'];
+       htmlOut += "</td></tr>";
+    }
+    if (popProperties['beschrijving werk'].length > 0 && popProperties['beschrijving werk'].length < 250 ){
+        htmlOut +="<tr class='popuptr'><td class='popuptd'><img alt='omschrijving' width='28px' src='img/info.png'></td><td>";
+        htmlOut += decodeURIComponent(popProperties['beschrijving werk'].toString().replace(/\+/g, ' '));
+       htmlOut += "</td></tr>";
+    }
+    if (popProperties['kunstvorm'].length > 0){
+        htmlOut +="<tr class='popuptr'><td class='popuptd'><img alt='kunstvorm' width='28px' src='img/art.png'></td><td>";
+        htmlOut += popProperties['kunstvorm'];
+        htmlOut += "</td></tr>";
+    }
+    if (popProperties['kraamnummer'].length > 0){
+        htmlOut +="<tr class='popuptr'><td class='popuptd'><img alt='kraamnummer' width='28px' src='img/art_gallery.png'></img></td><td>";
+        htmlOut += popProperties['kraamnummer'];
+       htmlOut += "</td></tr>";
+    }
+    htmlOut += "</tbody></table>";
+return htmlOut;
+    
+}
 
-/** var geojsonMarkerOptions = {
- radius: 4,
- fillColor: "#ff7800",
- color: "#000",
- weight: 1,
- opacity: 1,
- fillOpacity: 0.8
- };
- **/
-// Creates kraam marker
+
+// get popup data and create popup marker
 var mapMarker = L.icon({
     iconUrl: 'img/art_gallery.png',
-    iconSize: [20, 20], // size of the icon
+    iconSize: [30, 30], // size of the icon
     iconAnchor: [5, 5], // point of the icon which will correspond to marker's location
     popupAnchor: [0, -10] // point from which the popup should open relative to the iconAnchor
 
@@ -58,10 +102,10 @@ var mapMarker = L.icon({
 
 var directionMarker = L.icon({
     iconUrl: 'img/track.png',
-    iconSize: [20, 20], // size of the icon             
+    iconSize: [20, 20] // size of the icon             
 });
 var kraamPositions = [];
-var geojsonAjaxLayer = new L.GeoJSON.AJAX("data/km_map_json.json",
+var geojsonAjaxLayer = new L.GeoJSON.AJAX("http://www.kunstmarktdebilt.nl/km_app/km_map_json.php",
         {onEachFeature: popUp,
             pointToLayer: function (feature, latlng) {
                 var jsonMarker = L.marker(latlng, {icon: mapMarker});
@@ -75,73 +119,84 @@ geojsonAjaxLayer.addTo(map);
  * 
  * custom map controles for panning and automatic popup
  */
+/**
+ * event function for custom map control
+ * switch background image and disable/enable map pan control
+ * @param {event} e
+ * @returns {undefined}
+ */
+function changePan (e) {
+            if (panOn) {
+                panControlAdded.parentElement.style.backgroundImage = 'url(img/panning_off.png)';
+                panOn = false;
+            } else {
+                panControlAdded.parentElement.style.backgroundImage = 'url(img/panning_on.png)';
+                panOn = true;
+            }
+
+        }
 var panOn = true;
 var panControl = L.Control.extend({
     options: {
-        position: 'topleft'
+        position: 'bottomright'
     },
     onAdd: function (map) {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
 
         container.style.backgroundColor = 'white';
-        container.style.backgroundSize = '26px 26px';
+        container.style.backgroundSize = '35px 35px';
         container.style.backgroundImage = 'url(img/panning_on.png)';
-        container.style.width = '26px';
-        container.style.height = '26px';
-        container.innerHTML ='<a class=\"custom_icon_control" href=\"#\" title=\"automatisch verschuiven centrum kaart\"></a>';
-        
-
-        container.onclick = function () {
-            if (panOn) {
-                container.style.backgroundImage = 'url(img/panning_off.png)';
-                panOn = false;
-            } else {
-                container.style.backgroundImage = 'url(img/panning_on.png)';
-                panOn = true;
-            }
-
-        };
-
+        container.style.width = '35px';
+        container.style.height = '35px';
+        container.innerHTML ='<a id=\"pancontrol\" class=\"custom_icon_control\" href=\"#\" title=\"automatisch verschuiven centrum kaart\"></a>';
         return container;
     }
 });
 
 map.addControl(new panControl());
+var panControlAdded=document.getElementById('pancontrol');
+panControlAdded.addEventListener(clickEvent,changePan);
 
 //auto popup control
+/**
+ * event function for info control button
+ * changes the background image and disable/enable auto info
+ * @param {event} e
+ * @returns {undefined}
+ */
+function changeAutoinfo(e){
+    if (autoInfoOn) {
+        autoinfoAdded.parentElement.style.backgroundImage = 'url(img/information_off.png)';
+        autoInfoOn = false;
+    } else {
+        autoinfoAdded.parentElement.style.backgroundImage = 'url(img/information_on.png)';
+        autoInfoOn = true;
+    }
+}
 
 var autoInfoOn = true;
 var autoInfoControl = L.Control.extend({
     options: {
-        position: 'topleft'
+        position: 'bottomright'
     },
     onAdd: function (map) {
         var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
 
         container.style.backgroundColor = 'white';
-        container.style.backgroundSize = '26px 26px';
+        container.style.backgroundSize = '35px 35px';
         container.style.backgroundImage = 'url(img/information_on.png)';
-        container.style.width = '26px';
-        container.style.height = '26px';
-        container.innerHTML ='<a class=\"custom_icon_control" href=\"#\" title=\"automatische kraaminformatie\"></a>';
-        
-
-        container.onclick = function () {
-            if (autoInfoOn) {
-                container.style.backgroundImage = 'url(img/information_off.png)';
-                autoInfoOn = false;
-            } else {
-                container.style.backgroundImage = 'url(img/information_on.png)';
-                autoInfoOn = true;
-            }
-
-        };
+        container.style.width = '35px';
+        container.style.height = '35px';
+        container.innerHTML ='<a id=\"autoinfo\" class=\"custom_icon_control\" href=\"#\" title=\"automatische kraaminformatie\"></a>';
 
         return container;
     }
 });
 
 map.addControl(new autoInfoControl());
+//add event to control
+var autoinfoAdded=document.getElementById('autoinfo');
+autoinfoAdded.addEventListener(clickEvent,changeAutoinfo);
 
 /**
  * start dynamic location
@@ -176,16 +231,7 @@ function onLocationFound(actualPos) {
 
 }
 
-//map.on('locationfound', onLocationFound);
 
-/**	
- L.geoJson(geoJsonFeatureColl, {
- onEachFeature: onEachFeature,
- pointToLayer: function (feature, latlng) {
- return L.circleMarker(latlng, geojsonMarkerOptions);
- }			
- }).addTo(map);
- **/
 // onSuccess Callback
 //   This method accepts a `Position` object, which contains
 //   the current GPS coordinates
--- www/km_map_json.php
+++ www/km_map_json.php
@@ -1,25 +1,33 @@
 <?php
+// json_encode() options
+   @define('JSON_UNESCAPED_SLASHES',      64);   // Since PHP 5.4.0
+   @define('JSON_PRETTY_PRINT',           128);  // Since PHP 5.4.0
+   @define('JSON_UNESCAPED_UNICODE',      256);  // Since PHP 5.4.0
 
+   
 require_once dirname(__FILE__).'/scripts/dbconn.php';
 /**
  * @var
  */
-$fieldnames = array('Aanhef','Voornaam','Tussenvoegsel','Achternaam','Straat en huisnr', 'Postcode','Plaats','Website','Telefoon','Kunstvorm','Omschrijving van het werk');
+//$fieldnames = array('Aanhef','Voornaam','Tussenvoegsel','Achternaam','Straat en huisnr', 'Postcode','Plaats','Website','Telefoon','Kunstvorm','Omschrijving van het werk');
 //compose sql statement for user data
-$sql    = "SELECT w.id,w.krnr,w.email,w.publicatienaam,w.kunstvorm,w.krsoort,d.sub_id,d.field_name, d.field_val 
-    FROM wp_kmdbsubmissions w inner join wp_kmdbdata d on w.id = d.sub_id 
-    where w.marktjaar = '".$eventYear."' and (
-    d.field_name in ('".implode("','",$fieldnames)."')
-    or (d.field_name like 'Afbeelding%') and d.field_val <> '')
-    order by w.id asc;";
+$sql = "SELECT d.id, d.voornaam, d.voorvoegsel, d.achternaam,
+    a.straat, a.huisnr, a.postcode, a.plaats, l.omschrijving as land,
+    d.website, m.beschrijving_werk, m.kraamnummer, k.omschrijving as kunstvorm
+    FROM kmdb_deelnemer d inner join kmdb_deelnemer_markten m on (d.id = m.deeln_id)
+    inner join kmdb_kunstvorm k on (d.kunstvorm = k.id)
+    inner join kmdb_adres a on (d.adres = a.id)
+    inner join kmdb_land l on (a.land = l.id)
+    where m.markt_id = :kmyear and m.kraamnummer >0;";
 
-$sql_coordinate ="SELECT X, Y FROM km_krnr_loc where kmid = :kmidvalue";
-//debug command
-//echo $sql;
 
-//exit();
+$sql_coordinate ="SELECT X, Y FROM km_krnr_loc where krnr = :krnrvalue";
+
 
 $stmt = $dbh->prepare($sql);
+$stmt->bindParam('kmyear',$eventYear);
+
+
 //debug
 //var_dump($result);
 
@@ -27,63 +35,46 @@ $stmt = $dbh->prepare($sql);
 //create empty arrays for json data
 $featuresData = array();
 $propertiesData = array();
-$userid = 0;
-$userid_prev = 0;
+
 //get user data from db
 if ($stmt->execute()){
+    //debug command
+    //var_dump($stmt);
+
+    //exit();
     //loop db data
      while ($row = $stmt->fetch()) {
-         //set user id for grouping
-        $userid = $row['id'];
-        //when the user id did not changed get the next custom property
-        if ($userid == $userid_prev) {
-            $value = $row['field_name'];
-            //save only the predefined property
-            if (in_array($value,$fieldnames)){
-                $propertiesData[$value]= urlencode($row['field_val']);
-            }        
-        } 
-        // the user id is not equal a new set of user data is created
-        else {
-            //for the first user do not save the previous data (empty)
-            if (!empty($propertiesData)){
-                //save data from previous user id
-                //coordinate data
-               /*
-                * @todo  get coordinate data from db
-                */
-               $stcoordinate=$dbh->prepare($sql_coordinate);
-               $stcoordinate->bindParam('kmidvalue',$userid_prev);
-               $coordinateX= 0;
-               $coordinateY= 0;
-               
-               if ($stcoordinate->execute()){
-                   while ($coor_row = $stcoordinate->fetch()){
-                       $coordinateX =$coor_row['X'];
-                       $coordinateY =$coor_row['Y'];
-                   }
-               }
-               $coordinatesData = array( $coordinateX, $coordinateY );
+        $stcoordinate=$dbh->prepare($sql_coordinate);
+        $stcoordinate->bindParam('krnrvalue', $row['kraamnummer']);
+        $coordinateX= 0;
+        $coordinateY= 0;
 
-               $featuresData[]= array( "type" => "Feature", 
-                    "properties" => $propertiesData , 
-                    "geometry" => array ( "type" => "Point", 
-                        "coordinates" => $coordinatesData ) );
-            }          
-           //start with data for next user id 
-           $propertiesData = array (
-                    'id' => $row['id'],
-                    'krnr' => $row['krnr'],
-                    'email' => $row['email'],
-                    'kunstvorm' => $row['kunstvorm'],
-                    'krsoort' => $row['krsoort']);
-           
+        if ($stcoordinate->execute()){
+            while ($coor_row = $stcoordinate->fetch()){
+                $coordinateX =$coor_row['X'];
+                $coordinateY =$coor_row['Y'];
+            }
         }
-
-        $userid_prev = $userid;
-    }
+        $coordinatesData = array( $coordinateX, $coordinateY );
+    
+        $propertiesData = array (
+            'deelnemernummer' => $row['id'],
+            'kunstenaar' => $row['voornaam'].' '.$row['voorvoegsel'].' '.$row['achternaam'],
+            'adres' => $row['straat'].' '.$row['huisnr'].'</br >'.$row['postcode'].' '.$row['plaats'].'</br > '.$row['land'],
+            'website' => $row['website'],
+            'kunstvorm' => $row['kunstvorm'],
+            'beschrijving werk' => urlencode($row['beschrijving_werk']),
+            'kraamnummer' => $row['kraamnummer']
+           );
+        $featuresData[]= array( "type" => "Feature", 
+             "properties" => $propertiesData , 
+             "geometry" => array ( "type" => "Point", 
+                 "coordinates" => $coordinatesData ) );           
+        }
 }
 
+ 
+    
 //Leaflet json format for a json layer on a map
 //{
 //"type": "FeatureCollection",
@@ -95,7 +86,9 @@ if ($stmt->execute()){
 //		]
 //	}
 // json header
+header("Access-Control-Allow-Origin: *");
 header("Content-type: text/json; charset=utf-8");
+
 // encode all data to json
 echo json_encode(array( "type"=>"FeatureCollection", 
     "features" => $featuresData), JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
